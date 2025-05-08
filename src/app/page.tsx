85c2a68913c9f12b6ac21b26820e0411150c6d4e
WhittyGrehe <a21050008@unisierra.edu.mx>
2025-05-08 11:35:56 -0700

    Mis datos como participante

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,11 @@ const participants = [
     description: "Desarrollador Fullstack.",
     stack: "Laravel, Next.js, Nest.js, Vue.js, React, Java, MySQL, PHP, JavaScript, TypeScript, Git, GitHub, GitFlow, API Rest.",
   },
+  {
+    name: "WhittyGrehe",
+    description: "Ingeniero en Sistemas Computacionales",
+    stack: "Laravel, Next.js, Nest.js, Vue.js, React, Java, MySQL, PHP, JavaScript, TypeScript, Git, GitHub, GitFlow, API Rest.",
+  },
 ];
 
 export default function Home() {
