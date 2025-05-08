8d74d1cf4e26a14573b5d355d8d137e0e3c68af3
wImao <williamhenandezcastillo2000@gmail.com>
2025-05-08 11:36:48 -0700

    Change a dat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,11 @@ const participants = [
     description: "Desarrollador Fullstack.",
     stack: "Laravel, Next.js, Nest.js, Vue.js, React, Java, MySQL, PHP, JavaScript, TypeScript, Git, GitHub, GitFlow, API Rest.",
   },
+  {
+    name: "william",
+    description: "Developer",
+    stack: "Java, Python",
+  },
 ];
 
 export default function Home() {
